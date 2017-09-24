eeddd4bda908f2408d1ef0ba2f893e785fc83b28
Unknown <axiomprime@protonmail.com>
2017-09-24 12:12:38 -0700

    Add steemjs lib

---
+++ node_modules/steemjs-lib/dist/chain/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var TransactionBuilder = require("./src/TransactionBuilder");
+
+module.exports = {
+    TransactionBuilder: TransactionBuilder,
+
+    ChainTypes: require("./src/ChainTypes"),
+    ObjectId: require("./src/ObjectId"),
+    NumberUtils: require("./src/NumberUtils"),
+    TransactionHelper: require("./src/TransactionHelper"),
+    ChainValidation: require("./src/ChainValidation"),
+    Login: require("./src/AccountLogin")
+};
---
+++ node_modules/steemjs-lib/dist/chain/src/ChainConfig.js
@@ -0,0 +1,73 @@
+"use strict";
+
+var _this;
+
+var ecc_config = {
+    address_prefix: "STM"
+};
+
+module.exports = _this = {
+    core_asset: "STEEM",
+    vest_asset: "VESTS",
+    dollar_asset: "SBD",
+    address_prefix: "STM",
+    expire_in_secs: 15,
+    expire_in_secs_proposal: 24 * 60 * 60,
+    networks: {
+        Steem: {
+            core_asset: "STEEM",
+            address_prefix: "STM",
+            chain_id: "0000000000000000000000000000000000000000000000000000000000000000"
+        }
+    },
+    /** Set a few properties for known chain IDs. */
+    setChainId: function setChainId(chain_id) {
+
+        var i, len, network, network_name, ref;
+        ref = Object.keys(_this.networks);
+
+        for (i = 0, len = ref.length; i < len; i++) {
+
+            network_name = ref[i];
+            network = _this.networks[network_name];
+
+            if (network.chain_id === chain_id) {
+
+                _this.network_name = network_name;
+
+                if (network.address_prefix) {
+                    _this.address_prefix = network.address_prefix;
+                    ecc_config.address_prefix = network.address_prefix;
+                }
+
+                // console.log("INFO    Configured for", network_name, ":", network.core_asset, "\n");
+
+                return {
+                    network_name: network_name,
+                    network: network
+                };
+            }
+        }
+
+        if (!_this.network_name) {
+            console.log("Unknown chain id (this may be a testnet)", chain_id);
+        }
+    },
+
+    reset: function reset() {
+        _this.core_asset = "STEEM";
+        _this.address_prefix = "STM";
+        ecc_config.address_prefix = "STM";
+        _this.expire_in_secs = 15;
+        _this.expire_in_secs_proposal = 24 * 60 * 60;
+
+        console.log("Chain config reset");
+    },
+
+    setPrefix: function setPrefix() {
+        var prefix = arguments.length <= 0 || arguments[0] === undefined ? "STM" : arguments[0];
+
+        _this.address_prefix = prefix;
+        ecc_config.address_prefix = prefix;
+    }
+};
